b751e0581b1a24191078e3538e6c61ae3d35b674
Katie Lamb <katherinelamb16@gmail.com>
2021-07-20 12:26:32 -0600

    speed up preprocessing

--- scripts/ipcc/dcp30/preprocessing.py
+++ scripts/ipcc/dcp30/preprocessing.py
@@ -1,3 +1,4 @@
+# netCDF4 is a dependency of xarray, can just use netCDF4?
 import pandas as pd
 import xarray as xr
 import os
@@ -6,30 +7,36 @@ var_names_dict = {'pr': 'precipitation',
                  'tasmin': 'min_surface_air_temperature',
                  'tasmax': 'max_surface_air_temperature'}
 
-directory = 'mon/atmos'
+path = 'mon/atmos'
 ending_sub_dirs = 'r1i1p1/v1.0/CONUS'
 
+# clean up some of the filename/filepath naming here
 def write_csv(output, scenario):
     full_df = None
-    for var_name in os.listdir(os.path.join(scenario, directory)):
+    directory = os.path.join(scenario, path)
+    for var_name in os.listdir(directory):
         files_dir = os.path.join(directory, var_name, ending_sub_dirs)
         df_list_for_var = []
         for filename in os.listdir(files_dir):
             if filename.endswith('.nc'):
-                model_name = filename.split('_')[6]
-                ds = xr.open_dataset(filename)
+                print(filename)
+                filepath = os.path.join(files_dir, filename)
+                ds = xr.open_dataset(filepath)
+                model_name = ds.attrs['driving_model_id']
                 ds['time'] = ds.indexes['time'].normalize()
                 df = ds[var_name].to_dataframe()
+                print('read in dataframe')
                 df['model'] = model_name
-                df.set_index('model', append='True', inplace='True')
                 df_list_for_var.append(df)
+                break
+        full_df_for_var = pd.concat(df_list_for_var)
         if full_df is None:
-            full_df = pd.concat(df_list_for_var)
+            full_df = full_df_for_var
         else:
-            full_df = full_df.join(pd.concat(df_list_for_var))
-    full_df.rename(var_names_dict, inplace=True)
-    full_df.reset_index(inplace=True)
-    full_df.to_csv(output, index=False)
+            full_df = full_df.merge(full_df_for_var, on=['time', 'lat', 'lon', 'model'], how='outer')
+    full_df = full_df.rename(var_names_dict)
+    #full_df = full_df.reset_index()
+    full_df.to_csv(output)
 
 if __name__ == '__main__':
     write_csv('ipcc_dcp30_rcp85.csv', 'rcp85')
