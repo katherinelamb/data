535c204cb6e8c4233046d321cb41843149c15f0e
Katie Lamb <katherinelamb16@gmail.com>
2021-07-27 10:12:40 -0600

    added logging

--- scripts/ipcc/dcp30/dataflow_ipcc_preprocessing.py
+++ scripts/ipcc/dcp30/dataflow_ipcc_preprocessing.py
@@ -5,31 +5,33 @@ import os
 import xarray as xr
 import apache_beam as beam
 from google.cloud import storage
-from apache_beam.dataframe.convert import to_dataframe
-from apache_beam.dataframe.convert import to_pcollection
+#from apache_beam.dataframe.convert import to_dataframe
+#from apache_beam.dataframe.convert import to_pcollection
 from apache_beam.options.pipeline_options import PipelineOptions
 
 
-def netcdf_to_df(gcs_filepath, proj_name, bucket_name, scenario_name):
+def netcdf_to_df(gcs_filepath, proj_name, bucket_name):
+    logging.getLogger().setLevel(logging.INFO)
+    logging.info('beginning netcdf_to_df')
     client = storage.Client(project=proj_name)
     bucket = client.get_bucket(bucket_name)
     blob = bucket.blob(gcs_filepath) 
     blob.download_to_filename('temp.nc')
     ds = xr.open_dataset('temp.nc', engine='netcdf4')
+    logging.info(ds['time'][:5])
     # turn into a dataframe
     model_name = ds.attrs['driving_model_id']
     curr_var_name = ds.attrs['variableName']
     ds['time'] = ds.indexes['time'].normalize()
     df = ds[curr_var_name].to_dataframe()
+    logging.info(df[:5])
     df['model'] = model_name
-    df['scenario'] = scenario_name
     df['variable_name'] = curr_var_name
     df = df.rename(columns={curr_var_name: 'variable_value'})
-    
+    # change variable name to align with stat var name, add unit
+
     '''
-    # add in columns of 0s for other variables, this makes the to_dataframe and
-    # groupby calls work
-    # figure out what to do about nans
+    # add in columns of 0s for other variables
     for var in all_vars:
         if var != curr_var_name:
             df[var] = 0
@@ -40,13 +42,12 @@ def netcdf_to_df(gcs_filepath, proj_name, bucket_name, scenario_name):
     return csv_str
 
 def run(argv=None):
-    # take out variables argument, not needed
     parser = argparse.ArgumentParser()
     parser.add_argument('--bucket', default='unresolved_mcf')
     parser.add_argument('--prefix_start',
                 default='template_mcf_imports/nasa_ipcc/NEX-DCP30/BCSD/rcp85/mon/atmos')
-    parser.add_argument('--variables', default=['tasmax', 'tasmin', 'pr'])
-    parser.add_argument('--scenario', default='RCP8.5')
+    #parser.add_argument('--variables', default=['tasmax', 'tasmin', 'pr'])
+    #parser.add_argument('--scenario', default='RCP8.5')
     parser.add_argument('--prefix_end', default='r1i1p1/v1.0/test_data_small')
     parser.add_argument('--output', default='gs://unresolved_mcf/template_mcf_imports/nasa_ipcc/RCP85_csvs/processed')
     parser.add_argument('--project', default='datcom-204919')
@@ -77,7 +78,7 @@ def run(argv=None):
     # start pipeline
     with beam.Pipeline(options=options) as p:
         pc_files = p | beam.Create(input_files)
-        csv_strs = pc_files | beam.Map(netcdf_to_df, known_args.project, known_args.bucket, known_args.scenario)
+        csv_strs = pc_files | beam.Map(netcdf_to_df, known_args.project, known_args.bucket)
         '''
         df_schema = df_dicts | beam.Select(time=lambda item: item['time'], lat=lambda item: float(item['lat']),
                                             lon=lambda item: float(item['lon']), model=lambda item: str(item['model']),
